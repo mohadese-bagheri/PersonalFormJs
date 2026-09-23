75408997f99b603095e3ebc00eb505634df4fb54
mohi <zahra22mbagheri@gmail.com>
2026-09-23 23:59:08 +0330

    modal init

--- assests/main.js
+++ assests/main.js
@@ -25,5 +25,48 @@ form.addEventListener("submit", function (e) {
         gender: genderselect.value
     }
 
-    console.log(newperson);
+    if (!newperson.name || !newperson.family || !newperson.email) {
+        alert("لطفا فیلد های الزامی را پر کنید.");
+    }
+
+    people.push(newperson);
+    form.reset();
+
+
+    console.log(people);
 })
+
+document.getElementById("showmodal").addEventListener("click",
+    () => {
+        if (people.length == 0) { modal.innerHTML = `<h3> هنوز کاربری ثبت نشده است.</h3>`; }
+        else {
+            modal.innerHTML = `<h3> لیست کاربران :</h3> `;
+            const list = document.createElement("ul");
+
+            people.map((person, index) => {
+                const li = document.createElement("li");
+                li.innerText = ` ${index + 1} . ${person.name}  ${person.family}
+                ایمیل: ${person.email}
+                جنسیت: ${person.gender || `---`}
+                شغل: ${person.job || `---`}
+                شماره تماس: ${person.phone || `---`}`;
+                list.appendChild(li);
+            }
+
+            )
+            modal.appendChild(list);
+        }
+        overaly.style.opacity = "1";
+        overaly.style.visibility = "visible";
+        modal.style.opacity = "1";
+        modal.style.visibility = "visible";
+    }
+)
+
+overaly.addEventListener("click", () => {
+    overaly.style.opacity = "0";
+    overaly.style.visibility = "hidden";
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+
+});
