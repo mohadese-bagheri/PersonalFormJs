dd82eea53781cc8a618f591526777b7be98a5e46
mohi <zahra22mbagheri@gmail.com>
2026-09-25 14:57:23 +0330

    theend commit

--- assests/main.js
+++ assests/main.js
@@ -10,7 +10,7 @@ const modal = document.getElementById("modal");
 const overaly = document.getElementById("overaly");
 
 let people = [];
- 
+
 // افزودن کاربر جدید به لیست
 form.addEventListener("submit", function (e) {
     e.preventDefault()
@@ -51,7 +51,7 @@ document.getElementById("showmodal").addEventListener("click",
             // استفادع میکنیم برای همین از foreach 
             people.forEach((person, index) => {
 
-                const{name,family,email,job,phone,gender} = person;
+                const { name, family, email, job, phone, gender } = person;
                 const li = document.createElement("li");
                 li.innerText = ` ${index + 1} . ${person.name}  ${person.family}
                 ایمیل: ${person.email}
@@ -64,10 +64,7 @@ document.getElementById("showmodal").addEventListener("click",
             )
             modal.appendChild(list);
         }
-        overaly.style.opacity = "1";
-        overaly.style.visibility = "visible";
-        modal.style.opacity = "1";
-        modal.style.visibility = "visible";
+       handleshowmodal();
     }
 )
 
@@ -84,27 +81,60 @@ overaly.addEventListener("click", () => {
 
 // پیدا کردن با ایمیل
 document.getElementById("findByEmail").addEventListener("click",
-    ()=>{
-        const emailToFind =prompt("ایمیل مورد نظر را وارد کنید!");
-        const person = people.find( p => p.email === emailToFind.trim())
+    () => {
+        const emailToFind = prompt("ایمیل مورد نظر را وارد کنید!");
+        const person = people.filter(p => p.email === emailToFind.trim())
 
-        if(person) { 
-            const {name,family,email,job,phone,gender} = person;
-            modal.innerHTML=`
-            <h3> فرد مورد نظر یافت شد::</h3>
-            <p> نام :${name}</p>
-            <p>فامیلی: ${family} </p>
-            <p> شماره تماس: ${phone}</p>
-            <p> شغل: ${job} </p>
-            <p> جنسیت: ${gender} </p>
-            ` ; 
-        }else{
-            modal.innerHTML=`فرد مورد  نظر پیدا نشد.`;
+        if (person.length > 0) {
+            person.forEach((person, index) => {
+                const { name, family, email, job, phone, gender } = person;
+                modal.innerText = ` ${index + 1} . ${person.name}  ${person.family}
+                ایمیل: ${person.email}
+                جنسیت: ${person.gender || `---`}
+                شغل: ${person.job || `---`}
+                شماره تماس: ${person.phone || `---`}`;
+            });
+
+        } else {
+            modal.innerHTML = `فرد مورد  نظر پیدا نشد.`;
         }
 
-        overaly.style.opacity="1";
-        overaly.style.visibility="visible";
-        modal.style.opacity="1";
-        modal.style.visibility="visible";
+        handleshowmodal();
     }
 )
+
+// آیا همه شاغلند 
+document.getElementById("checkIFAllHaveJob").addEventListener(
+    "click", () => {
+        const allhavejob = people.every(p => p.job.trim() !== "");
+        modal.innerHTML = allhavejob
+            ? " <p> ✅همه افراد دارای شغل هستند</p>"
+            : " <p> ❌ همه افراد دارای شغل نیستند</p>";
+
+
+        handleshowmodal();
+    }
+)
+
+// آیا حداقل یک مرد هست؟
+document.getElementById("checkIFAnyHasMen").addEventListener(
+    "click", () => {
+
+        const hasmen = people.some(p => p.gender === "مرد");
+
+        modal.innerHTML = hasmen
+            ? " <p>حداقل یک آقا ثبت شده است. </p>"
+            : " <p>هیچ آقایی ثبت نشده است❌ </p>";
+
+        handleshowmodal();
+
+    }
+)
+
+const handleshowmodal = () => {
+    overaly.style.opacity = "1";
+    overaly.style.visibility = "visible";
+
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+}
